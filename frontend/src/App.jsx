b19aa4182b5b381fee8688ddfc36cfcdf01ba5f5
Mahes Krishna <maheshkrishnabandla773@gmail.com>
2023-11-29 14:47:40 -0500

    Done some changes in App js and added register js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,45 @@
 
-
-import './App.css'
-import Home from './components/Home';
 import React, { useEffect, useState } from "react";
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import NoPage from './components/NoPage';
-import Login from './components/Login';
-import Register from './components/Register';
-import NavBar from './components/NavBar';
-import ApiService from './api/testService';
-import Profile from './components/Profile';
-import JobApplicationForm from'./components/JobApplicationForm';
-import JobList from './components/JobList';
+import { Suspense, lazy } from 'react';
+import Home from "./pages/Home";
+
 function App() {
   
+ const HomePg = lazy(()=> import("./pages/Home"));
 
+ const LoginPg = lazy(()=> import("./pages/Login"));
+
+ 
+ const RegisterPg = lazy(()=> import("./pages/Register"));
+ const listjobs = lazy(()=> import("./pages/ListJobs"));
+ const job = lazy(()=> import("./pages/Job"));
+ const profile = lazy(()=> import("./pages/MyProfile"));
 
   return (
     
-    <Router>
 
+<Router>
+<Suspense>
+  <Routes>
+  <Route path="/Login" Component={LoginPg} />
   
-      <Routes>
-      
-      <Route path='*' element={<NoPage />} />
+  <Route path="/register" Component={RegisterPg} />
+
+  <Route path="/" Component={HomePg} />
+  <Route path="/listjobs" Component={listjobs} />
+  <Route path="/myprofile" Component={profile} />
+  <Route path="/job/:jobid" Component={job} />
+  
+  
 
 
-      <Route path="/" element={<Home />} />
-      
-      
-      
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/job_application" element={<JobApplicationForm />} />
-      <Route path="/job_list" element={<JobList />} />
-      
-      </Routes>
-    </Router>
+
+ 
+  </Routes>
+</Suspense>
+  </Router>
+
   )
 }
 
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import "../css/Login.css"
+import { Link } from "react-router-dom"
+import axios from 'axios';
+function Register(){
+    
+    const [ermsg,setErmsg] = useState("");
+
+        const [email,setEmail] = useState("")
+        const [firstname,setFirstname] = useState("")
+        const [lastname,setLastname] = useState("")
+        const [password,setPassword] = useState("")    
+
+        const [isValid,setIsValid] = useState(true)
+
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        var data = {email:email,firstname:firstname,lastname:lastname,password:password}
+
+            axios.post('http://localhost:3000/register',data).then(response => {
+                console.log(response)
+                setIsValid(false)
+                setEmail("")
+                setFirstname("")
+                setLastname("")
+                setPassword("")
+            })
+            .catch(error => {
+                console.error('Error sending data:', error);
+           });
+
+
+
+      };
+
+    return (
+        <>
+        <div className="gettingStartedLogo">
+        <h1>Talent Acquisition Hub</h1>
+        </div>
+        <section className="form-section divLoginOuter">
+            <h1 className="homepage-heading">Talent Acquisition Hub</h1>
+            <div className={`successMsg ${isValid ? 'hide' : 'show'}`}>
+            Registered Successfully
+            </div>
+
+        <form onSubmit={handleSubmit}>
+            <input required value={firstname} onChange={(e)=>  setFirstname(e.target.value)}  type="text" placeholder="First Name" />
+            <input required type="text" placeholder="Last Name"  value={lastname} onChange={(e)=>  setLastname(e.target.value)} />
+            <input required type="text" placeholder="Email ID"  value={email} onChange={(e)=>  setEmail(e.target.value)}/>
+            <input required type="password" placeholder="Password" value={password} onChange={(e)=>  setPassword(e.target.value)} />
+            <button type="submit" className="btn btn-primary">Sign Up</button>
+        </form>
+        <div className="loginFoot">
+        <p className="white-link">Already have an account?<Link to="/Login"><span className="white-link white-link-dec"> Sign in here </span></Link> </p>
+        </div>
+    </section>
+        </>
+    )
+}
+export default Register
