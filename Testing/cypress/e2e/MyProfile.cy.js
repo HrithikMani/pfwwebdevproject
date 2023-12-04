b9c2474c4afbb24dccfee607a86bba4e24dc9f91
navya0803 <142939112+navya0803@users.noreply.github.com>
2023-12-04 07:33:44 -0500

    update

--- Testing/cypress/e2e/MyProfile.cy.js
+++ Testing/cypress/e2e/MyProfile.cy.js
@@ -30,25 +30,18 @@ describe('ListJobs Component', () => {
   it('should navigate to the login page when clicking on Logout', () => {
     cy.get('a[href="/login"]').click();
     cy.url().should('include', '/login');
-    // Add more assertions specific to the login page if needed
   });
 
   it('should apply for a job', () => {
-    // Assuming there is at least one job listing
     cy.get('.jobbox').first().within(() => {
       cy.get('button.btnApply').click();
     });
     cy.url().should('include', '/job/');
-    // Add more assertions specific to the job application page if needed
   });
 
   it('should review application from My Applications modal', () => {
-    // Assuming there is at least one application in My Applications modal
     cy.contains('My Applications').click();
     cy.get('#MyAppplications').should('be.visible');
     cy.get('.btn-warning').first().click();
-    // Add more assertions specific to the application review page if needed
   });
-
-  // Add more test cases based on your component's functionality
 });
